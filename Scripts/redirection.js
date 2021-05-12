82e92a00c267e3e8fb442381bb4decff27d1e395
Ethan Diosana <danielrenendez@gmail.com>
2021-05-12 12:11:11 -0700

    Implemented page redirection, fixed index styling.

--- Scripts/redirection.js
+++ Scripts/redirection.js
@@ -1,10 +1,16 @@
+
+
 $(document).ready(function () {
 
-
+    /* On button click, redirect to index.html */
     $("#submitButton").click(function () {
-        console.log("submit button clicked.");
-        $.ajax({
 
-        });
+        console.log("submit button clicked.");
+
+        $.get('/getIndex',{ '+': $.now()}
+        
+        ).done(function (data){
+            $("#body").html(data);
+        }) 
     });
 })
