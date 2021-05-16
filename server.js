8454fd976f0ecdca2e5302733735f4b501221536
John Braithwaite <jwbraith@gmail.com>
2021-05-15 23:19:11 -0700

    installed express-session, further work on the /authorize POST

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const http = require('http');
 const path = require('path');
@@ -8,6 +9,17 @@ const server = http.createServer(app);
 app.use(express.static(path.join(__dirname, 'Images')));
 app.use(express.static(path.join(__dirname, 'Styles')));
 app.use(express.static(path.join(__dirname, 'Scripts')));
+
+app.use(session(
+  {
+    secret: 'the unaltered void beyond all things',
+    name: 'zamSessionID',
+    resave: false,
+    saveUninitialized: true
+  }
+))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 //GET REQUESTS
 
@@ -21,15 +33,44 @@ app.get('/getIndex', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+
 // POST trying to sign into a profile
 app.post('/authorize', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
+  console.log(`Username: ${req.body.username}, Password: ${req.body.password}`);
+  let results = authenticate(req.body.username, req.body.password, function (rows) {
+    if (rows == null) {
+      // no users found
+      res.send({
+        status: "fail",
+        msg: 'User account not found.'
+      });
+    } else {
+      // authenticate the user, create a session
+      req.session.loggedIn = true;
+      req.session.username = rows.username;
+      req.session.save(function (err) {
+        // session is saved
+      })
 
-  console.log("Username: ", req.body.username);
-  console.log("Password: ", req.body.password);
+
+      res.send({
+        status: 'success',
+        msg: 'Logged in.'
+      });
+    }
+  })
 })
 
 // SERVER LISTENING
 server.listen(8000, () => {
   console.log('listening on localhost:8000');
 });
+
+
+function authenticate(user, pwd, callback) {
+  const mysql = require('mysql2');
+  const connection = mysql.createConnection({
+
+  })
+}
